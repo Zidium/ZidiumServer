294b4e7f0cd241197576a3291ee0b7748691c018
Zidium <info@zidium.net>
2019-09-04 17:02:10 +0300

    Исправлен файл сборки релиза

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -12,18 +12,18 @@ rmdir /s /q Release
 "%msbuild.exe%" UserAccount\UserAccount.csproj /t:Build /p:Configuration=Release /v:minimal /p:DeployOnBuild=true /p:PublishProfile=CreateRelease.pubxml
 
 "%msbuild.exe%" Agent\Agent.csproj /t:Build /p:Configuration=Release /v:minimal
-xcopy /E /Y /S Agent\bin\Release\*.pdb Release\Agent\
-xcopy /E /Y /S Agent\bin\Release\NLog.config Release\Agent\
-xcopy /E /Y /S Agent\bin\Release\Zidium.config Release\Agent\
-xcopy /E /Y /S Agent\App.Release.config Release\Agent\Zidium.Agent.exe.config*
-xcopy /E /Y /S Agent\bin\Release\*.exe Release\Agent\
-xcopy /E /Y /S Agent\bin\Release\*.dll Release\Agent\
+xcopy /Y Agent\bin\Release\*.pdb Release\Agent\
+xcopy /Y Agent\bin\Release\NLog.config Release\Agent\
+xcopy /Y Agent\bin\Release\Zidium.config Release\Agent\
+xcopy /Y Agent\App.Release.config Release\Agent\Zidium.Agent.exe.config*
+xcopy /Y Agent\bin\Release\*.exe Release\Agent\
+xcopy /Y Agent\bin\Release\*.dll Release\Agent\
 
 "%msbuild.exe%" Tools\DatabasesUpdate\DatabasesUpdate.csproj /t:Build /p:Configuration=Release /v:minimal
-xcopy /E /Y /S Tools\DatabasesUpdate\bin\Release\*.pdb Release\DatabasesUpdate\
-xcopy /E /Y /S Tools\DatabasesUpdate\bin\Release\NLog.config Release\DatabasesUpdate\
-xcopy /E /Y /S Tools\DatabasesUpdate\bin\Release\DatabasesUpdate.exe.config Release\DatabasesUpdate\
-xcopy /E /Y /S Tools\DatabasesUpdate\bin\Release\*.exe Release\DatabasesUpdate\
-xcopy /E /Y /S Tools\DatabasesUpdate\bin\Release\*.dll Release\DatabasesUpdate\
+xcopy /Y Tools\DatabasesUpdate\bin\Release\*.pdb Release\DatabasesUpdate\
+xcopy /Y Tools\DatabasesUpdate\bin\Release\NLog.config Release\DatabasesUpdate\
+xcopy /Y Tools\DatabasesUpdate\bin\Release\DatabasesUpdate.exe.config Release\DatabasesUpdate\
+xcopy /Y Tools\DatabasesUpdate\bin\Release\*.exe Release\DatabasesUpdate\
+xcopy /Y Tools\DatabasesUpdate\bin\Release\*.dll Release\DatabasesUpdate\
 
 pause
