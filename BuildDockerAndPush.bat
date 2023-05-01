27e3cfc371996ff5bd53fe64594fdc709725467b
Zidium <info@zidium.net>
2023-05-01 19:20:21 +0300

    v2.4.0

--- BuildDockerAndPush.bat
+++ BuildDockerAndPush.bat
@@ -1,5 +1,5 @@
 @echo off
 docker login
-docker build -t zidiumteam/zidium-simple:2.3.0 -t zidiumteam/zidium-simple:latest .
-docker push zidiumteam/zidium-simple:2.3.0
+docker build -t zidiumteam/zidium-simple:2.4.0 -t zidiumteam/zidium-simple:latest .
+docker push zidiumteam/zidium-simple:2.4.0
 docker push zidiumteam/zidium-simple:latest
